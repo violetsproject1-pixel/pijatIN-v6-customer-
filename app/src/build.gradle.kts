3c512cfc06e2ffb6352f430bb12c97028c0bbfe3
violetsproject1-pixel <violetsproject1@gmail.com>
2026-08-23 19:47:57 +0700

    Update build.gradle.kts

--- app/src/build.gradle.kts
+++ app/src/build.gradle.kts
@@ -2,33 +2,27 @@ plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
 }
-
 android {
     namespace = "com.pijatin.customer"
-    compileSdk = 35
-
+    compileSdk = 34
     defaultConfig {
         applicationId = "com.pijatin.customer"
-        minSdk = 23
-        targetSdk = 35
-        versionCode = 6
-        versionName = "6.0"
+        minSdk = 24
+        targetSdk = 34
+        versionCode = 63
+        versionName = "7.4"
     }
-
-    buildTypes {
-        release {
-            isMinifyEnabled = false
-            isShrinkResources = false
-            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
-        }
-        debug {
-            applicationIdSuffix = ".debug"
-            versionNameSuffix = "-debug"
-        }
-    }
+    buildFeatures { compose = true }
+    composeOptions { kotlinCompilerExtensionVersion = "1.5.8" }
+    packaging { resources { excludes += "/META-INF/{AL2.0,LGPL2.1}" } }
 }
-
 dependencies {
-    implementation("androidx.core:core-ktx:1.15.0")
-    implementation("androidx.appcompat:appcompat:1.7.0")
+    implementation("androidx.core:core-ktx:1.12.0")
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.7.0")
+    implementation("androidx.activity:activity-compose:1.8.2")
+    implementation(platform("androidx.compose:compose-bom:2024.02.00"))
+    implementation("androidx.compose.ui:ui")
+    implementation("androidx.compose.ui:ui-graphics")
+    implementation("androidx.compose.ui:ui-tooling-preview")
+    implementation("androidx.compose.material3:material3")
 }
