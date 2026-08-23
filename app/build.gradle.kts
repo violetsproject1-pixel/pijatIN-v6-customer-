81409b8681370cc7c898c8576445072485eef84a
violetsproject1-pixel <violetsproject1@gmail.com>
2026-08-23 22:59:21 +0700

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -42,4 +42,5 @@ dependencies {
     implementation("androidx.compose.ui:ui-graphics")
     implementation("androidx.compose.ui:ui-tooling-preview")
     implementation("androidx.compose.material3:material3") 
+implementation("io.coil-kt:coil-compose:2.5.0")
 }
