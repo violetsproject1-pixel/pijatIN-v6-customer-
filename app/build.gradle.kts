3730f33390a522eedf5cd510e88bee7b500a9891
violetsproject1-pixel <violetsproject1@gmail.com>
2026-08-23 19:21:18 +0700

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,7 +11,7 @@ namespace = "com.pijatin.customer"
  applicationId = "com.pijatin.customer"
         minSdk = 24
         targetSdk = 34
-        versionCode = 52
+        versionCode = 58
         versionName = "7.2"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
